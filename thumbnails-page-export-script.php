918ba689fda297544fffc2f15b57f5a9d102ef90
JACGWD <gwd@johnabbott.qc.ca>
2019-02-03 14:54:37 -0500

    Update thumbnails-page-export-script.php

--- thumbnails-page-export-script.php
+++ thumbnails-page-export-script.php
@@ -1,36 +1,53 @@
 <!doctype html>
- <html class="no-js" lang="en">
+ <html class="no-js" lang="">
 
  <head>
    <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <link rel="canonical" href="" />
    <title>Photo Gallery</title>
-   <meta name="description" content="Student Photo Gallery">
+   <meta name="description" content="Photo Gallery">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+   <link rel="manifest" href="site.webmanifest">
+   <link rel="apple-touch-icon" href="icon.png">
+   <!-- Place favicon.ico in the root directory -->
+
+   <link rel="stylesheet" href="css/normalize.css">
+   <link rel="stylesheet" href="css/main.css">
  </head>
 
  <body>
 
+   <nav>
+     <!-- head ends here -->
+
+
+
+     <!-- for each image start here -->
+
+   <!-- <li><a href="<?php echo $filename . ".php" ?>"<?php echo $filename ?></a> -->
+
+      <li><a href="{fileNameNoExtension}.php"><img src="img/{fileNameNoExtension}.jpg" width="200" alt="{title}"></a></li>
+
+   <!-- for each image ends here -->
+
+
+   <!-- foot starts here -->
+   </nav>
+
+
+<h1>Photo Gallery</h1>
 
    <main>
 
-     <h1>Photo Gallery</h1>
+<h2>Some other content would go on the page here</h2>
 
-     <nav>
-<!-- head ends here -->
-
-
-<!-- <li><a href="<?php echo $filename . ".php" ?>"<?php echo $filename ?></a> -->
-
-        <li><a href="{fileNameNoExtension}.php">{fileNameNoExtension}</a></li>
-<!-- for each image ends here -->
-
-
-<!-- foot starts here -->
-     </nav>
 
 
    </main>
+
+
 
  </body>
 
