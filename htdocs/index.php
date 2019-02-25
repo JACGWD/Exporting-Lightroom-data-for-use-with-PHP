6d14449d0fa90c0c0c7ed58ef804ae0ffd69bb8d
JACGWD <gwd@johnabbott.qc.ca>
2019-02-24 20:34:51 -0500

    Add .gitignore and index.php example

---
+++ htdocs/index.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<title>Photo Gallery... 70 characters or less. Put keywords at the beginning.</title>
+
+<meta name="description" content="Photo portfolio by (your name). Images blablabla... limit
+of 155 characters. Put keywords at the beginning.">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="manifest" href="site.webmanifest">
+
+<link rel="apple-touch-icon" href="icon.png">
+
+<link rel="stylesheet" href="/themes/my-theme/css/simple-css-reset.css" media="all" />
+<link rel="stylesheet" href="/themes/my-theme/css/style.css?v=2" media="all" />
+
+<meta name="robots" content="noodp, noydir">
+
+<meta property="author" content="(your name)">
+<link href="https://fonts.googleapis.com/css?family=Armata|Changa" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+    <p class="headerTitle">Your Name Design</p>
+    <p class="headerSubtitle">Photography Portfolio</p>
+</header>
+
+<main>
+<h1>2018 Photo Gallery</h1>
+
+<figure class="thumb">
+
+<a href="000137-083.php"><img src="img/000137-083_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000132-019.php"><img src="img/000132-019_200px.jpg" width="200" alt="Winter plant abstract" /></a>
+
+<figcaption>Dried and frozen stalks draw crisp abstract lines against the background pattern of bands of snow on the frozen surface of the Lake of Two Mountains at Jack Layton Park in Hudson, Quebec, Canada.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000045-033.php"><img src="img/000045-033_200px.jpg" width="200" alt="Stream going through a forest in autumn" /></a>
+
+<figcaption>Viviry creek and foot bridge in fall. Hudson, Quebec, Canada.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="19.155.2.172.php"><img src="img/19.155.2.172_200px.jpg" width="200" alt="Sunrise at Anse de Vaudreuil, Vaudreuil-Dorion, Quebec" /></a>
+
+<figcaption>Mid-summer sunrise over the Lake of Two Mountains at Anse de Vaudreuil, Vaudreuil-Dorion, Quebec, Canada.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000006-075.php"><img src="img/000006-075_200px.jpg" width="200" alt="Sunset over the Lake of Two Mountains" /></a>
+
+<figcaption>Sunset over Anse de Vaudreuil looking towards Oka, Quebec</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000067-009.php"><img src="img/000067-009_200px.jpg" width="200" alt="Tree reflection and Canada goose on a foggy morning" /></a>
+
+<figcaption>Reflection of a tree in the early morning still waters of the Lake of Two-Mountains  at Jack Layton Memorial Park in Hudson, Quebec, Canada.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-0104.php"><img src="img/000001-0104_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-0351.php"><img src="img/000001-0351_200px.jpg" width="200" alt="Red maple in fog" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-0095.php"><img src="img/000001-0095_200px.jpg" width="200" alt="Echinacea flower" /></a>
+
+<figcaption>Closeup detail view of an Echinacea (echinacea purpurea) flower.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-0080.php"><img src="img/000001-0080_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000231-042-Pano.php"><img src="img/000231-042-Pano_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000231-002.php"><img src="img/000231-002_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000204-0006.php"><img src="img/000204-0006_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000136-002.php"><img src="img/000136-002_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000132-025.php"><img src="img/000132-025_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000045-098.php"><img src="img/000045-098_200px.jpg" width="200" alt="Bright red maple tree in fall" /></a>
+
+<figcaption>Maple tree with red leaves against an overcast sky in autumn</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-1412-2.php"><img src="img/000001-1412-2_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000001-0267.php"><img src="img/000001-0267_200px.jpg" width="200" alt="Viviry creek near the lake of Two-Mountains. Sandy Beach, Hudson, Quebec." /></a>
+
+<figcaption>Viviry creek near the lake of Two-Mountains. Sandy Beach, Hudson, Quebec.</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="000068-053.php"><img src="img/000068-053_200px.jpg" width="200" alt="" /></a>
+
+<figcaption>Some sample text goes here</figcaption>
+</figure>
+
+
+<figure class="thumb">
+
+<a href="P-023-0001.php"><img src="img/P-023-0001_200px.jpg" width="200" alt="Girl on a floating dock at Oka park, Quebec" /></a>
+
+<figcaption>Young girl on floating dock in Oka national park, Oka Quebec Canada</figcaption>
+</figure>
+
+</main>
+
+<footer>
+&copy; Your Name, 2019. All rights reserved for published and non-published works.
+</footer>
+
+<script src="/themes/my-theme/js/modernizr-custom.js"></script>
+
+<script src="https://code.jquery.com/src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+<script>window.jQuery || document.write('<script src="/themes/my-theme/js/jquery-3.3.1.min.js"><\/script>')</script>
+
+<script src="/themes/my-theme/js/plugins.js"></script>
+
+<script src="/themes/my-theme/js/main.js"></script>
+
+</body>
+</html>
